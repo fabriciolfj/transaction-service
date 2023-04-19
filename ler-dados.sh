986d87cf91847f57f93c733fb6b18dbdccd1756a
fabricio jacob <fabrluc@outlook.com>
2023-04-19 13:00:58 -0300

    add

--- ler-dados.sh
+++ ler-dados.sh
@@ -1,3 +1,3 @@
 aws dynamodb --endpoint-url http://localhost:4566 get-item --consistent-read \
     --table-name transaction \
-    --key '{ "code": {"S": "538092c7-ba4e-421b-ae81-3bca2d50ccb5"}, "status": {"S": "approved"}}'
+    --key '{ "code": {"S": "1220f72b-b806-4551-b64d-14253f425a09"}, "status": {"S": "approved"}}'
